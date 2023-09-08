54053d026e0758614c55ceeaef3f3d91e9c9dea4
Oriol <oriol@berlinches.com>
2023-09-08 04:36:53 +0200

    Final changes, added title and color picker

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-let counter = 0;
+// Initialize counter
+let counter = localStorage.getItem('counter') || 0;
+document.getElementById('counter').innerText = counter;
 
-if (localStorage.getItem('counter')) {
-  counter = parseInt(localStorage.getItem('counter'));
-  document.getElementById('counter').innerText = counter;
-}
+// Initialize color
+let color = localStorage.getItem('color') || '#ffffff';
+document.getElementById('colorPicker').value = color;
+document.getElementById('colorPicker').style.backgroundColor = color;
+document.getElementById('titleInput').style.backgroundColor = color;
+document.querySelector('.widget-container').style.backgroundColor = color;
+
+
+// Initialize title
+let title = localStorage.getItem('title') || '';
+document.getElementById('titleInput').value = title;
+document.title = title;
 
 function increment() {
   counter++;
-  updateCounter();
+  document.getElementById('counter').innerText = counter;
+  localStorage.setItem('counter', counter);
 }
 
 function decrement() {
   counter--;
-  updateCounter();
+  document.getElementById('counter').innerText = counter;
+  localStorage.setItem('counter', counter);
 }
 
 function reset() {
   counter = 0;
-  updateCounter();
-}
-
-function updateCounter() {
   document.getElementById('counter').innerText = counter;
   localStorage.setItem('counter', counter);
+}
+
+function changeColor() {
+  color = document.getElementById('colorPicker').value;
+  document.querySelector('.widget-container').style.backgroundColor = color;
+  document.getElementById('colorPicker').style.backgroundColor = color;
+  document.getElementById('titleInput').style.backgroundColor = color;
+  localStorage.setItem('color', color);
+}
+
+
+function changeTitle() {
+  title = document.getElementById('titleInput').value;
+  document.title = title;
+  localStorage.setItem('title', title);
 }
